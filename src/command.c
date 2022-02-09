efc6fada74b2ef72142ca8ed0a76476bfed356ec
Chuxiao Feng <chuxiaof@uchicago.edu>
2022-02-09 14:22:58 -0600

    use safe strncmp

--- src/command.c
+++ src/command.c
@@ -1,9 +1,10 @@
 #include "command.h"
-
 #include <string.h>
 
 #include "log.h"
 #include "handler.h"
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
 
 typedef int (*handler_func)(context_handle ctx, user_handle user_info, message_handle msg);
 
@@ -35,8 +36,9 @@ int handlers_num = sizeof(handler_entries) / sizeof(struct handler_entry);
 int process_cmd(context_handle ctx, user_handle user_info, message_handle msg)
 {
     int i;
+    int len=strlen(msg->cmd);
     for (i = 0; i < handlers_num; i++) {
-        if (!strcmp(msg->cmd, handler_entries[i].command_name)) {
+        if (!strncmp(msg->cmd, handler_entries[i].command_name, MAX(len, strlen(handler_entries[i].command_name)))) {
             return handler_entries[i].func(ctx, user_info, msg);
         }
     }
--- src/user.h
+++ src/user.h
@@ -5,41 +5,39 @@
 #include <uthash.h>
 
 /**
- * @brief Store the relevant information about user, 
+ * @brief Store the relevant information about user,
    also contains the socket file descriptor of the connection
- * 
+ *
  */
-struct user_t
+typedef struct user_t
 {
-    // the socket of connection
-    int client_fd;
-    char *client_host_name;
-    
-    char *nick;
-    char *username;
-    char *fullname;
-    bool registered;
-    bool is_irc_operator;
-    
-    // makes this structure hashable
-    UT_hash_handle hh; 
-};
+  // the socket of connection
+  int client_fd;
+  char *client_host_name;
 
-typedef struct user_t user_t;
+  char *nick;
+  char *username;
+  char *fullname;
+  bool registered;
+  bool is_irc_operator;
 
-typedef user_t * user_handle;
+  // makes this structure hashable
+  UT_hash_handle hh;
+} user_t;
+
+typedef user_t *user_handle;
 
 /**
  * @brief Create a user object
- * 
- * @return user_handle 
+ *
+ * @return user_handle
  */
 user_handle create_user();
 
 /**
  * @brief free the memory
- * 
- * @param user 
+ *
+ * @param user
  */
 void destroy_user(user_handle user);
 
@@ -47,10 +45,10 @@ void destroy_user(user_handle user);
  * @brief to check whether we can register current user
  * if both nick and username fields have value, the answer is true,
  * false otherwise
- * 
- * @param user 
- * @return true 
- * @return false 
+ *
+ * @param user
+ * @return true
+ * @return false
  */
 bool can_register(user_handle user);
 
